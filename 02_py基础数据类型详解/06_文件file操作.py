c9bfd86ec2134a5afa3f72f43e9b5895e9a1b3e3
lap_lp <lp_lap_glb@88.com>
2022-10-21 18:28:48 +0800

    20221021晚上地铁，学习

--- 02_py基础数据类型详解/06_文件file操作.py
+++ 02_py基础数据类型详解/06_文件file操作.py
@@ -2,10 +2,12 @@
 1. 打开文件
 open(路径,操作模式mode,编码encoding)
     路径：绝对路径（从根目录出发）；相对路径（相对当前目录）
-    mode:缺省；  r=read  w=write  a=append b=binary(二进制非文本文件)
-    encoding:缺省
+    mode:缺省为r；  r=read  w=write  a=append b=binary(二进制非文本文件)
+    encoding:缺省win是gbk  mac是utf-8
 with()打开的文件，会自动管理连接
 """
+import os  # 和操作系统相关的模块
+import time  # 和时间相关的模块
 # 读
 # f = open("../source/note.txt", mode="r", encoding="utf-8")  # 打开文件，并还没有加载到内存
 # content = f.read()  # 全部读取
@@ -32,3 +34,9 @@ with()打开的文件，会自动管理连接
 with open("../source/11.png", mode="rb") as f1,  open("../source/12.png", mode="wb") as f2:
     for line in f1:
         f2.write(line)
+# 修改文件  先读，再写如新文件；新文件就是修改后的文件
+time.sleep(3)   # 休眠3秒
+os.remove("../source/11.png")  # 删除文件
+time.sleep(3)   # 休眠3秒
+os.rename("../source/12.png", "../source/11.png")  # 重新命名
+
