6b54a0ef8778d3f1363de7ea29be8ee96bf0c064
lap_lp <lp_lap_glb@88.com>
2022-10-23 20:18:22 +0800

    20221023，学习-迭代器

--- 03_函数function/04_装饰器.py
+++ 03_函数function/04_装饰器.py
@@ -7,25 +7,106 @@
 """
 
 
+# def guanjia(game):
+#     # def inner(username, password):
+#     def inner(*args, **kwargs):  # args是元组，kwargs是dict
+#         print("打开外挂")
+#         # game(username, password)
+#         game(*args, **kwargs)  # 打散再返回回去
+#         print("关闭外挂")
+#
+#     return inner
+#
+#
+# @guanjia  # 相当于paly_wz = guanjia(paly_wz)
+# def paly_wz(username, password):
+#     print("鲁班哪里走", username, password)
+#
+#
+# @guanjia
+# def paly_lolo(username, password, hero):
+#     print("德玛西亚！！！", username, password, hero)
+
+
+# paly_wz = guanjia(paly_wz)
+# paly_lolo = guanjia(paly_lolo)
+# paly_wz("lp", "123")
+# paly_lolo("lp", "123", "盖伦")
+
+# ==============返回值============
+
 def guanjia(game):
-    def inner():
+    # def inner(username, password):
+    def inner(*args, **kwargs):  # args是元组，kwargs是dict
         print("打开外挂")
-        game()
+        # game(username, password)
+        ret = game(*args, **kwargs)  # 打散再返回回去
         print("关闭外挂")
+        return ret
 
     return inner
 
 
 @guanjia  # 相当于paly_wz = guanjia(paly_wz)
-def paly_wz():
-    print("鲁班哪里走")
+def paly_wz(username, password):
+    print("鲁班哪里走", username, password)
+    return "100元"
 
 
 @guanjia
-def paly_lolo():
-    print("德玛西亚！！！")
+def paly_lolo(username, password, hero):
+    print("德玛西亚！！！", username, password, hero)
+    return "99元"
 
 
 # paly_wz = guanjia(paly_wz)
-paly_lolo = guanjia(paly_lolo)
-paly_wz()
+# paly_lolo = guanjia(paly_lolo)
+print(paly_wz("lp", "123"))
+print(paly_lolo("lp", "123", "盖伦"))
+
+login_flag = False
+
+
+def login_verify(fn):
+    def inner(*args, **kwargs):
+        global login_flag
+        if not login_flag:
+            print("还未完成登录操作")
+            # 登录验证
+            while 1:
+                username = input(">>>")
+                password = input(">>>")
+                if username == "lp" and password == "123":
+                    print("登陆成功")
+                    login_flag = True
+                    break
+                else:
+                    print("登陆失败,请重新登录！")
+        ret = fn(*args, **kwargs)
+        return ret
+
+    return inner
+
+
+@login_verify
+def add():
+    print("添加员工信息")
+
+
+@login_verify
+def delete():
+    print("删除员工信息")
+
+
+@login_verify
+def update():
+    print("更新员工信息")
+
+
+def search():
+    print("添加查询员工信息")
+
+
+add()
+delete()
+update()
